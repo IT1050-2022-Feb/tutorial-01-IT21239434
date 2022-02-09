7f768219992a8b88c955b83c4e98d864c5b2517b
Kalpa punchihewa <99349489+IT21239434@users.noreply.github.com>
2022-02-09 16:45:06 +0000

    completed 1

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,22 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  int sub1 ,sub2 , total = 0 , average = 0 ;//variable declaration
+
+  printf("Enter mark 1 : ");
+  scanf("%d" , &sub1);
+
+  printf("Enter mark 2 : ");
+  scanf("%d" , &sub2);
+
+  total = sub1 + sub2 ;
+  average = total / 2 ;
+
+  printf("Total is : %d \n" , total);
+  printf("Average is : %d" , average);
+
   return 0;
 }
 
